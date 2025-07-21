8a4451a65c111d7ccb7c91b3c97e58e8334a820b
Mohit <mohitsharma8864@gmail.com>
2025-07-21 21:54:09 +0530

    feat: setup lint and prettier commands

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,30 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettier, // Disable ESLint rules that conflict with Prettier
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'writable',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -23,7 +32,32 @@ export default defineConfig([
       },
     },
     rules: {
+      // Code quality rules
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'no-alert': 'warn',
+
+      // React specific rules
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // Best practices
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'no-undef': 'error',
+
+      // Code style (these will be handled by Prettier)
+      indent: 'off',
+      quotes: 'off',
+      semi: 'off',
+      'comma-dangle': 'off',
+      'arrow-parens': 'off',
+      'bracket-spacing': 'off',
+      'jsx-quotes': 'off',
+
+      // Disable fast refresh rule for files that export both components and utilities
+      'react-refresh/only-export-components': 'off',
     },
   },
-])
+]);
